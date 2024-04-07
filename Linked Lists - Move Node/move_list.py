28872b7632c28344ce01bd4645e17905441302d5
NazarM <melnyk.pn@ucu.edu.ua>
2024-04-07 22:53:58 +0300

    Linked Lists - Move Node

---
+++ Linked Lists - Move Node/move_list.py
@@ -0,0 +1,13 @@
+class Node(object):
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+    
+class Context(object):
+    def __init__(self, source, dest):
+        self.source = source
+        self.dest = dest
+    
+def move_node(source, dest):
+  if source is None: raise ValueError
+  return Context(source.next, Node(source.data, dest))
